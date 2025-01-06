925ae7ab3ab5fd7370e18c13ae1a81fdfb3e5896
ipa23.m.dzerve <ipa23.m.dzerve@pjvt.local>
2025-01-06 09:48:27 +0200

    CHECKPOINT 06.01.2025

--- index.php
+++ index.php
@@ -1,15 +1,32 @@
 <?php
 
+// Mērķis: izveidot blogu meklētāju.
+
 require "functions.php";
 require "Database.php";
 
 $config = require("config.php");
 
+echo "<h1>Blogs</h1>";
 $db = new Database($config["database"]);
 $posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 /*$comments = $db->query("SELECT * FROM comments")->fetchAll(PDO::FETCH_ASSOC);
 $user = = $db->query("SELECT * FROM users WHERE user_id = $id")->fetch(PDO::FETCH_ASSOC);
 $db->query("INSERT INTO posts");*/
+
+if (isset($_GET["search_query"]) && $_GET["search_query"] !="") {
+    // Meklēšanas loģika
+    dd("SELECT * FROM posts WHERE content LIKE '" . $_GET["search_query"] . "';");
+    $posts = $db->query("SELECT * FROM posts WHERE content LIKE " . $_GET["search_query"])->fetchAll();
+}
+
+// Meklēšanas forma
+// POST - maina datu bāzē saturu
+// GET - atlasa datus
+echo "<form>";
+echo "<input name='search_query' />";
+echo "<button>Meklēt</button>";
+echo"</form>";
 
 echo "<ul>";
 foreach ($posts as $post) {
