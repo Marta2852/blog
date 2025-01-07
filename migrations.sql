e2700a8c824eff844e117eda622b49e3f67c81c4
ipa23.m.dzerve <ipa23.m.dzerve@pjvt.local>
2025-01-07 11:12:48 +0200

    07.01.2025 update categories

--- migrations.sql
+++ migrations.sql
@@ -14,3 +14,15 @@ VALUES
 ("Otrais bloga ieraksts");
 
 SELECT * FROM posts;
+
+CREATE TABLE categories(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	category_name VARCHAR(25)
+	);
+	
+	INSERT INTO categories
+	(category_name)
+	VALUES
+	("Svētki"),
+	("Mūzika"),
+	("Sports");
