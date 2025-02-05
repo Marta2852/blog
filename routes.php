9aded03c30eb2759a164e4d93b0545d46dd3af32
ipa23.m.dzerve <ipa23.m.dzerve@pjvt.local>
2025-02-05 11:26:36 +0200

    edit files

--- routes.php
+++ routes.php
@@ -3,6 +3,7 @@
 return [
     "/" => "controllers/posts/index.php",
     "/show" => "controllers/posts/show.php",
+    "/edit" => "controllers/posts/edit.php",
     "/about" => "controllers/story.php",
     "/create" => "controllers/posts/create.php"
 ];
